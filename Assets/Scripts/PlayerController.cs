c69c69bc3d2a0fdc9d80eb3468bc25827d27bcdf
Mjones090 <145160703+Mjones090@users.noreply.github.com>
2023-09-29 22:05:49 +0100

    Code clean up and hierarchy clean up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    //Private Variables
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
@@ -25,6 +27,8 @@ public class PlayerController : MonoBehaviour
         // move the vehicle forward
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // We turn the vehicle
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
       
     }
